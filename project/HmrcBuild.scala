fdf4921b46afad3c217411e0731857bd7fe54e89
Duncan Crawford <duncan@noughtsandones.co.uk>
2015-08-12 21:12:26 +0100

    increment simple-reactivemongo

--- project/HmrcBuild.scala
+++ project/HmrcBuild.scala
@@ -42,7 +42,7 @@ object Dependencies {
   object Compile {
     val scalaTest = "org.scalatest" %% "scalatest" % "2.2.4" % "provided"
     val pegdown = "org.pegdown" % "pegdown" % "1.5.0" % "provided"
-    val simpleReactiveMongo = "uk.gov.hmrc" %% "simple-reactivemongo" % "3.1.1" % "provided"
+    val simpleReactiveMongo = "uk.gov.hmrc" %% "simple-reactivemongo" % "3.1.2"
     val playJson = "com.typesafe.play" %% "play-json" % "2.3.9" % "provided"
   }
 
