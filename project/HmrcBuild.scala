6a1340778a4f48c23fc4792c5426e7c6fbe031fd
Howy Perrin <howyp@users.noreply.github.com>
2015-11-20 16:35:45 +0000

    Upgrade simple-reactivemongo to 4.3.0
    Also remove `provided` from HMRC dependencies

--- project/HmrcBuild.scala
+++ project/HmrcBuild.scala
@@ -41,8 +41,8 @@ object Dependencies {
   object Compile {
     val scalaTest = "org.scalatest" %% "scalatest" % "2.2.4" % "provided"
     val pegdown = "org.pegdown" % "pegdown" % "1.5.0" % "provided"
-    val simpleReactiveMongo = "uk.gov.hmrc" %% "simple-reactivemongo" % "4.2.1" % "provided"
-    val playJson = "com.typesafe.play" %% "play-json" % "2.3.10" % "provided"
+    val simpleReactiveMongo = "uk.gov.hmrc" %% "simple-reactivemongo" % "4.3.0"
+    val playJson = "com.typesafe.play" %% "play-json" % "2.3.10"
   }
 
   sealed abstract class Test(scope: String) {
